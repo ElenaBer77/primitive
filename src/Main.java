9c5c1cf794b48a08f376897c39d8f851e3ca618b
ElenaBer77 <pei281277@gmail.com>
2024-03-25 18:46:01 +0300

    correction

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ticket = 13676; //стоимость билета
-        int oneBonus = 20; //количество бонусов
-        int bonus = ticket / oneBonus;
-        System.out.println(bonus + "  начислено бонусных миль");
+        int ticket = 14009; //стоимость билета
+        int oneBonus = 20; //количество рублей на одну бонусную милю
+        int bonus = ticket / oneBonus; //расчетное количество бонусных миль
+        System.out.println(bonus + "-бонусных миль");
     }
 }
